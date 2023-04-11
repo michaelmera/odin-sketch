319c8058d416ac11709ffc630f9a15431e0b36df
Michael Mera <michael.mera@cispa.de>
2023-04-11 15:10:55 +0200

    feat: change background color on mouse over

--- sketch.js
+++ sketch.js
@@ -4,4 +4,10 @@ for (let i = 0; i < 16 * 16; i++) {
     const cell = document.createElement('div');
     cell.classList.add('cell');
     grid.appendChild(cell);
+
+    cell.addEventListener('mouseover', (e) => color(e.target));
+}
+
+function color(cell) {
+    cell.style.backgroundColor = '#000';
 }
